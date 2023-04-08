48cdb2132dd6f0331bb74fa62a3abede236a2d00
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2023-04-08 21:07:15 +0530

    Leetcode solutio added

---
+++ DSALeetcode/Climbing_Stairs.java
@@ -0,0 +1,15 @@
+//Leetcode 70. Climbing Stairs
+//https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+    public int climbStairs(int n) {
+        int prev=1;
+        int prev2=1;
+        for(int i=2;i<=n;i++){
+            int cur=prev2+prev;
+            prev2=prev;
+            prev=cur;
+        }
+        return prev;
+    }
+}
