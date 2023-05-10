7c0e83dcdbb692bb7e4ddd269611226fabca7056
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2023-05-10 09:40:04 +0530

    Create Evaluate_Reverse_Polish_Notation.java

---
+++ DSALeetcode/Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,27 @@
+//Leetcode 150. Evaluate Reverse Polish Notation 
+//https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st=new Stack<>();
+        
+        for(String str:tokens){
+            if("+-/*".contains(str)){
+                int val1=st.pop();
+                int val2=st.pop();
+                int res=calc(val2,val1,str);
+                
+                st.push(res);
+            }else st.push(Integer.parseInt(str));
+        }
+        return st.pop();
+    }
+    public int calc(int val1,int val2,String str){
+        int res=0;
+        if(str.equals("+")) res=val1+val2;
+        else if(str.equals("-")) res= val1-val2;
+        else if(str.equals("*")) res= val1*val2;
+        else if(str.equals("/")) res= val1/val2;
+        return res;
+    }
+}
